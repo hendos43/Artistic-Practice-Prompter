71cc1f98baf811790157d82b09d93b32de573ba4
Stephen <>
2024-11-10 13:07:00 +0800

    super basic auth test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 from google_auth_oauthlib.flow import Flow
 
 # Initialize OAuth flow using environment variables
@@ -12,3 +13,10 @@ client_config = {
 }
 
 flow = Flow.from_client_config(client_config, scopes=['https://www.googleapis.com/auth/drive.file'])
+
+st.title("Google Drive Uploader")
+st.write("This app allows you to upload files to Google Drive.")
+st.write("Please authenticate with Google Drive to continue.")
+st.write("Click the link below to authenticate with Google Drive.")
+auth_url, _ = flow.authorization_url()
+st.write(f"[Click here to authenticate]({auth_url})")
